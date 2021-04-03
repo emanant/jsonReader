b4caaf6c324b270eeaee0f08e7d9d7b3ff0e1ee2
Manan Gajjar <something.nothing01@gmail.com>
2021-04-03 23:22:25 +0530

    add home page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAVlKdjBZk82UKAS0edEbuCScxncMueBos',
+    authDomain: 'learning-69006.firebaseapp.com',
+    projectId: 'learning-69006',
+    storageBucket: 'learning-69006.appspot.com',
+    messagingSenderId: '883834714271',
+    appId: '1:883834714271:web:db9b2137b57b789ba0caaa',
+    measurementId: 'G-VG38KDCRC6',
+  },
 };
